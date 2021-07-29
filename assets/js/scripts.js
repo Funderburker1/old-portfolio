7f08278c81b774a34548e406268035ec8a97f968
devjoaocamargo <joaovscamargo01@gmail.com>
2021-07-29 19:42:20 -0300

    feat/contact

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -38,3 +38,7 @@ document.addEventListener('DOMContentLoaded', function() {
    var elems = document.querySelectorAll('.carousel');
    var instances = M.Carousel.init(elems);
  });
+
+ var instance = M.Carousel.init({
+   fullWidth: true
+ });
